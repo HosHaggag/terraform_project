579d334e5a8c499ea17d3f409be846ff075e79f0
haggagdev <haggagdev@gmail.com>
2023-10-02 23:28:56 +0300

    hot fix

--- lambda_function.py
+++ lambda_function.py
@@ -1,46 +1,27 @@
-import os
 import boto3
-import json
-from datetime import datetime
 
-from_email = "haggagdev@gmail.com"
-config_set_name = "sen_email"
-client = boto3.client('ses')
+def lambda_handler(to_address, subject):
+    # Create an SES client
+    client = boto3.client('ses')
 
-
-def lambda_handler(event, context):
-
-    body_html = f"""<html>
-        <head></head>
-        <body>
-          <h2>Email from AWS SES!</h2>
-          <br/>
-          <p>This was sent from a Python Lambda using boto3</p> 
-        </body>
-        </html>
-                    """
-
-    email_message = {
-        'Body': {
-            'Html': {
-                'Charset': 'utf-8',
-                'Data': body_html,
-            },
-        },
-        'Subject': {
-            'Charset': 'utf-8',
-            'Data': "Hello from AWS SES",
-        },
-    }
-
-    ses_response = client.send_email(
+    # Send the email
+    response = client.send_email(
+        Source='haggagdev@gmail.com',
         Destination={
-            'ToAddresses': ['haggagdev@gmail.com'],
+            'ToAddresses': [
+                'haggagdev@gmail.com',
+            ]
         },
-        Message=email_message,
-        Source=from_email,
-        ConfigurationSetName=config_set_name,
+        Message={
+            'Subject': {
+                'Data': 'New Version of Terraform Applyed'
+            },
+            'Body': {
+                'Text': {
+                    'Data': 'New Version of Terraform Applyed check out here : https://github.com/HosHaggag/terraform_project.git'
+                }
+            }
+        }
     )
-
-    print(f"ses response id received: {response['MessageId']}.")
-
+    
+    return response
